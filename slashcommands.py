444a4185e1badb828e2a2d70bd5831b4fb89a8be
Joshua <joshbear367763@gmail.com>
2025-01-23 21:44:08 -0500

    Added descriptions to slash commands. Regrettably, I lied about the sync command

--- slashcommands.py
+++ slashcommands.py
@@ -20,12 +20,12 @@ async def runtime(interaction : discord.Interaction):
 	ct = time.localtime(start_time) # current time
 	await interaction.response.send_message(f"I came online at {ct.tm_mon}/{ct.tm_mday}/{ct.tm_year} {ct.tm_hour}:{ct.tm_min}:{ct.tm_sec}, and have been running for {int(time.time() - start_time)} seconds!!!") #noqa
 
-@bot.tree.command(name="say")
+@bot.tree.command(name="say", description="Make me say something!")
 @app_commands.describe(thing_to_say="What should I say?")
 async def say(interaction: discord.Interaction, thing_to_say: str):
 	await interaction.response.send_message(f"{thing_to_say}") # noqa
 
-@bot.tree.command(name="pig-latin")
+@bot.tree.command(name="pig-latin", description="Write Pig-Latin!")
 @app_commands.describe(phrase="What to pig-latinize")
 async def piggy(interaction: discord.Interaction, phrase: str):
 	if len(phrase) < 1:
@@ -42,7 +42,7 @@ async def piggy(interaction: discord.Interaction, phrase: str):
 
 
 
-@bot.tree.command(name="download-to-host")
+@bot.tree.command(name="download-to-host", description='Download a YouTube video to {config["DEVELOPER_USERNAME"]}\'s computer!')
 @app_commands.describe(yt_link="Link to a YouTube video")
 async def download_to_host(interaction: discord.Interaction, yt_link : str):
 	if interaction.user.name == config["ANNOYING_FRIEND_USERNAME"]:
@@ -52,7 +52,7 @@ async def download_to_host(interaction: discord.Interaction, yt_link : str):
 		await interaction.response.send_message("Attempting download....")
 		asyncio.create_task(yt.yt_download(yt_link, interaction))
 
-@bot.tree.command(name="sync")
+@bot.tree.command(name="sync", description='Only for {config["DEVELOPER_USERNAME"]}! Synchronizes slash commands with your server.')
 async def sync(interaction : discord.Interaction):
 	if interaction.user.name != config["DEVELOPER_USERNAME"]:
 		await interaction.response.send_message(f"You are not worthy to use this command {interaction.user.nick if interaction.user.nick is not None else interaction.user.global_name}")
@@ -60,7 +60,7 @@ async def sync(interaction : discord.Interaction):
 
 	await interaction.response.send_message(f"Attempting to sync slash commands to {interaction.guild}...")
 	try:
-		synced = await bot.tree.sync(interaction.guild_id)
+		synced = await bot.tree.sync()
 	except Exception as e:
 		await interaction.edit_original_response(content="Failed to sync commands")
 		print(f'[ERROR]:\tFailed to sync commands: {e}')
@@ -68,7 +68,7 @@ async def sync(interaction : discord.Interaction):
 	print(f"Just attempted to sync {len(synced)} commands")
 	await interaction.edit_original_response(content=f"Synchronized {len(synced)} slash commands to {interaction.guild}...")
 
-@bot.tree.command(name="hop-on")
+@bot.tree.command(name="hop-on", description="Get me in call!")
 async def hop_on(interaction : discord.Interaction):
 	global vc
 	voice = interaction.user.voice
