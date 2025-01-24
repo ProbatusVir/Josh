a071e4fd18d72988d424fd54ecf16740d60c8499
Joshua <joshbear367763@gmail.com>
2025-01-23 21:33:47 -0500

    yt_download now works asynchronously to prevent thread locking. Downloading would stop other bot interactions.

--- yt.py
+++ yt.py
@@ -1,13 +1,18 @@
 from pytubefix import YouTube
 from dotenv import dotenv_values
 import pytubefix
+from discord import Interaction
 
 config = dotenv_values()
 
-async def yt_download(link : str, file_format : str ='mp4'):
+
+
+#TODO: Make this return codes to be handled for decoupling.
+async def yt_download(link : str, interaction : Interaction, file_format : str ='mp4'):
 	try:
 		yt = YouTube(link, use_oauth=True)
 	except Exception as e:
+		await interaction.edit_original_response(content="It appears the link is invalid.")
 		print(f"Uh oh. Something went wrong Opening YouTube link!\t{e}")
 		return False
 
@@ -18,8 +23,11 @@ async def yt_download(link : str, file_format : str ='mp4'):
 		d_video.download(output_path=config["YT_SAVE_PATH"], filename=f'{yt.title}.{file_format}', skip_existing=True)
 		print(f"Successfully downloaded {yt.title}")
 	except Exception as e:
+		await interaction.edit_original_response(content="Something went wrong downloading video!")
 		print(f"Uh oh! Something went wrong downloading the video!\t{e}")
 		return False
+
+	await interaction.edit_original_response(content=f'Successfully downloaded {link} ! We"re testing this message right now.')
 	return True
 
 
