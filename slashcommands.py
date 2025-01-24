80aa6b072e0c744c24fcbc5dffde82b59111e741
Joshua <joshbear367763@gmail.com>
2025-01-24 16:33:42 -0500

    Added a search slash command, working on a play-file

--- slashcommands.py
+++ slashcommands.py
@@ -2,15 +2,30 @@ import asyncio
 import time
 
 import discord
-from discord import app_commands
+from discord import app_commands, File
 import yt
+
+import discord.opus
 
 import declarations
 bot = declarations.bot
 config = declarations.config
-start_time = declarations.start_time
 
+# I'm realizing now that I need a bot class to extend the base.
+start_time = declarations.start_time
 vc : discord.VoiceClient = None
+
+async def join_voice(interaction : discord.Interaction) -> bool:
+	global vc
+	if interaction.user.voice is None:
+		return False
+	channel = interaction.user.voice.channel
+	if channel is not None:
+		vc = await channel.connect(self_deaf=True)
+		return True
+	else:
+		return False
+
 
 @bot.tree.command(
 	name="uptime",
@@ -42,8 +57,8 @@ async def piggy(interaction: discord.Interaction, phrase: str):
 
 
 
-@bot.tree.command(name="download-to-host", description='Download a YouTube video to {config["DEVELOPER_USERNAME"]}\'s computer!')
-@app_commands.describe(yt_link="Link to a YouTube video")
+@bot.tree.command(name="download-to-host", description=f'Download a YouTube video to {config["DEVELOPER_USERNAME"]}\'s computer!')
+@app_commands.describe(yt_query="Accepts a YT link or a search")
 async def download_to_host(interaction: discord.Interaction, yt_link : str):
 	if interaction.user.name == config["ANNOYING_FRIEND_USERNAME"]:
 		await interaction.response.send_message(f"{config["ANNOYING_FRIEND_NAME"]}STOP!")
@@ -52,7 +67,7 @@ async def download_to_host(interaction: discord.Interaction, yt_link : str):
 		await interaction.response.send_message("Attempting download....")
 		asyncio.create_task(yt.yt_download(yt_link, interaction))
 
-@bot.tree.command(name="sync", description='Only for {config["DEVELOPER_USERNAME"]}! Synchronizes slash commands with your server.')
+@bot.tree.command(name="sync", description=f'Only for {config["DEVELOPER_USERNAME"]}! Synchronizes slash commands with your server.')
 async def sync(interaction : discord.Interaction):
 	if interaction.user.name != config["DEVELOPER_USERNAME"]:
 		await interaction.response.send_message(f"You are not worthy to use this command {interaction.user.nick if interaction.user.nick is not None else interaction.user.global_name}")
@@ -68,11 +83,23 @@ async def sync(interaction : discord.Interaction):
 	print(f"Just attempted to sync {len(synced)} commands")
 	await interaction.edit_original_response(content=f"Synchronized {len(synced)} slash commands to {interaction.guild}...")
 
+@bot.tree.command(name="search", description="Get the top result of YouTube search")
+@app_commands.describe(query="Text to search YouTube for")
+async def search(interaction : discord.Interaction, query : str):
+	await interaction.response.send_message("Searching the tubes...")
+	await yt.yt_search(query, interaction)
+
 @bot.tree.command(name="hop-on", description="Get me in call!")
 async def hop_on(interaction : discord.Interaction):
-	global vc
-	voice = interaction.user.voice
-	channel_to_join = voice.channel if voice is not None else None
-	await interaction.response.send_message("Aight bro, I'm hopping on." if channel_to_join is not None else "Bro... you tricked me... you're not even in call...")
-	if channel_to_join is not None:
-		vc = await channel_to_join.connect(self_deaf=True)
+	joined = await join_voice(interaction)
+	await interaction.response.send_message("Aight bro, I'm hopping on." if joined else "Bro... you tricked me... you're not even in call...")
+
+@bot.tree.command(name="play-file", description="Play a file! WIP")
+async def play_file(interaction : discord.Interaction, file: discord.Attachment):
+	joined = await join_voice(interaction)
+	if not joined:
+		await interaction.response.send_message(f"There was no channel to join.")
+		return
+
+	await interaction.response.send_message(f"Playing {file.filename}")
+	#vc.play()
