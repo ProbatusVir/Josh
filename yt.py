44af44dbf713415b0beba5d32d871fb54bc0050b
Joshua <joshbear367763@gmail.com>
2025-01-26 17:32:44 -0500

    Implemented caching of sorts

--- yt.py
+++ yt.py
@@ -1,3 +1,4 @@
+import io
 import re
 
 from pytubefix import YouTube
@@ -10,17 +11,57 @@ import pytubefix.innertube
 
 config = dotenv_values()
 
+delim : str = '|'
+
+def get_vid_name(video : str) -> str:
+	return video.split('\\')[-1][:-4]
+
+def find_in_cache(query : str) -> str | None:
+	if query.find(delim) != -1:
+		return ""
+	try:
+		cache = io.open(f'{config["YT_SAVE_PATH"]}/cache.txt', 'rt')
+	except Exception as e:
+		print(e)
+		return None #cache probably doesn't exist. Or is just plain inaccessible, either way, not that deep.
+
+	for line in cache:
+		if len(line) == 0: # would break parsing
+			continue
+		values = line.split(delim)
+		if values[0] == query:
+			return values[1][:-1]
+	return None
+
+def write_to_cache(query : str, filename : str):
+	cache = io.open(f'{config["YT_SAVE_PATH"]}/cache.txt', 'a')
+	cache.write(f'{query}{delim}{filename}\n')
+
+
+
 #TODO: Make this return codes to be handled for decoupling.
-async def yt_download(link : str, interaction : Interaction, file_format : str ='mp4') -> str | None:
-	link_data = re.search("(?:v=|\\/)([0-9A-Za-z_-]{11}).*", link)
+async def yt_download(query : str, interaction : Interaction, file_format : str = 'mp4') -> str | None:
+	query = query.lower().strip()
+	cached = find_in_cache(query)
+	if cached == "":
+		await interaction.edit_original_response(content=f"Invalid character {delim} in query.")
+	elif cached is not None:
+		path = f"{config["YT_SAVE_PATH"]}/{cached}.{file_format}"
+		#path = cached
+		print(f"Found cached file: {path}")
+		return path
+	else:
+		print("Did not find a cached file.")
+
+	link_data = re.search("(?:v=|\\/)([0-9A-Za-z_-]{11}).*", query)
 	if link_data is not None:
 		try:
-			yt = YouTube(link, use_oauth=True)
+			yt = YouTube(query, use_oauth=True)
 		except Exception as e:
 			print(f"[ERROR]: {e}")
 			return None
 	else:
-		yt = await yt_search(link, interaction, command=False)
+		yt = await yt_search(query, interaction, command=False)
 
 	mp4_streams = yt.streams.filter(file_extension=file_format)
 	d_video: pytubefix.streams.Stream = mp4_streams[-1] # The last one is the highest resolution
@@ -29,11 +70,10 @@ async def yt_download(link : str, interaction : Interaction, file_format : str =
 		path = d_video.download(output_path=config["YT_SAVE_PATH"], filename=f'{yt.title}.{file_format}', skip_existing=True)
 		print(f"Successfully downloaded {yt.title}")
 	except Exception as e:
-		await interaction.edit_original_response(content="Something went wrong downloading video!")
 		print(f"Uh oh! Something went wrong downloading the video!\t{e}")
 		return None
 
-	await interaction.edit_original_response(content=f'Successfully downloaded {yt.title} !')
+	write_to_cache(query, get_vid_name(path))
 	return path
 
 async def yt_search(query : str, interaction : Interaction, command : bool = True) -> YouTube:
